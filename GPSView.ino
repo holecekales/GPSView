f0215118cead9830ab0ff656dd2f3f958268c9a7
alesh <ales@outlook.com>
2017-08-05 00:31:51 -0700

    Navigation module working

--- GPSView.ino
+++ GPSView.ino
@@ -57,9 +57,10 @@ uint8_t prevDisplayMode  = -1;
 char printBuffer[15];
 
 // Navigation data
-float WP_LAT = 0;
-float WP_LON = 0;
-uint8_t WP_OK = 0;
+
+float WP_LAT = 46.626636;       // (sample point)
+float WP_LON = -124.060092;
+uint8_t WP_OK = 1;
 boolean WP_Capture = false;
 
 //------------------------------------------------------------------------------
@@ -172,7 +173,7 @@ void displayContext(const char* screen) {
     oled.print(screen);
   }
 
-  if(displayMode != DM_MOTION)
+  if(displayMode != DM_MOTION && displayMode != DM_NAVI)
     displayTimefromGPS(3,7);  
 }
 
@@ -195,6 +196,51 @@ void displayMotion() {
     oled.print("Ang:"); oled.println(dtostrf(GPS.angle,6,1,printBuffer));
     oled.print("Alt:"); oled.println(dtostrf(GPS.altitude,6,1,printBuffer));
   }
+}
+
+//------------------------------------------------------------------------------
+void displayNavigation() {
+  if(WP_OK == 0) {
+    oled.setCursor(12, 3);
+    oled.set2X();
+    oled.print("Waypoint?"); 
+    return;
+  }
+
+  const uint8_t o = 7;
+
+  if(GPS.fix) {
+      oled.setCursor(0, 2);
+      oled.set2X();
+      oled.print("Dst:"); 
+      oled.setCol(oled.col() + o);
+      float dist = distance_between(GPS.latitudeDegrees, GPS.longitudeDegrees, WP_LAT, WP_LON);
+      char* u = "m";
+      if(dist >= 1000) {
+        u = "km";
+        dist /= 1000.f;
+      }
+      oled.print(dtostrf(dist,5,1,printBuffer));
+      oled.set1X();
+      oled.print(u);
+      oled.set2X();
+      oled.setCursor(0, 4);
+      oled.print("Crs:"); 
+      float crs = course_to(GPS.latitudeDegrees, GPS.longitudeDegrees, WP_LAT, WP_LON);
+      oled.setCol(oled.col() + o);
+      oled.print(dtostrf(crs,5,1,printBuffer));
+      oled.set1X();
+      oled.print("o");
+      oled.set2X();
+      oled.setCursor(0, 6);
+      oled.print("Dir: ");
+      oled.setCol(oled.col() + o);
+      oled.clearToEOL();
+      oled.println(cardinal(crs));
+      // maybe add a difference in altitude
+  }
+    
+
 }
 
 //------------------------------------------------------------------------------
@@ -234,6 +280,11 @@ void updateDisplay() {
         }
     break;
     case DM_NAVI:
+        if (redraw || (millis() - timer > 500)) { 
+          timer = millis(); // reset the timer  
+          displayContext("Navigate");
+          displayNavigation();
+        }
     break;
   }
 }
@@ -321,4 +372,62 @@ void handleButton() {
     WP_LAT = GPS.latitudeDegrees;
     WP_LON = GPS.longitudeDegrees;
   }
+}
+
+
+//------------------------------------------------------------------------------
+float distance_between (float lat1, float long1, float lat2, float long2) 
+{
+  // returns distance in meters between two positions, both specified 
+  // as signed decimal-degrees latitude and longitude. Uses great-circle 
+  // distance computation for hypothetical sphere of radius 6372795 meters.
+  // Because Earth is no exact sphere, rounding errors may be up to 0.5%.
+  // Courtesy of Maarten Lamers
+  float delta = radians(long1-long2);
+  float sdlong = sin(delta);
+  float cdlong = cos(delta);
+  lat1 = radians(lat1);
+  lat2 = radians(lat2);
+  float slat1 = sin(lat1);
+  float clat1 = cos(lat1);
+  float slat2 = sin(lat2);
+  float clat2 = cos(lat2);
+  delta = (clat1 * slat2) - (slat1 * clat2 * cdlong); 
+  delta = sq(delta); 
+  delta += sq(clat2 * sdlong); 
+  delta = sqrt(delta); 
+  float denom = (slat1 * slat2) + (clat1 * clat2 * cdlong); 
+  delta = atan2(delta, denom); 
+  return delta * 6372795; 
+}
+
+
+//------------------------------------------------------------------------------
+float course_to (float lat1, float long1, float lat2, float long2) 
+{
+  // returns course in degrees (North=0, West=270) from position 1 to position 2,
+  // both specified as signed decimal-degrees latitude and longitude.
+  // Because Earth is no exact sphere, calculated course may be off by a tiny fraction.
+  // Courtesy of Maarten Lamers
+  float dlon = radians(long2-long1);
+  lat1 = radians(lat1);
+  lat2 = radians(lat2);
+  float a1 = sin(dlon) * cos(lat2);
+  float a2 = sin(lat1) * cos(lat2) * cos(dlon);
+  a2 = cos(lat1) * sin(lat2) - a2;
+  a2 = atan2(a1, a2);
+  if (a2 < 0.0)
+  {
+    a2 += TWO_PI;
+  }
+  return degrees(a2);
+}
+
+//------------------------------------------------------------------------------
+const char* cardinal (float course)
+{
+  static const char* directions[] = {"N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW"};
+
+  int direction = (int)((course + 11.25f) / 22.5f);
+  return directions[direction % 16];
 }
